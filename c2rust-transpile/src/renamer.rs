038a80ab4ad6cdf889a6e5551d34f343ec61f5da
Rua <ruawhitepaw@gmail.com>
2026-01-15 22:23:12 +0100

    transpile: Assert that newly picked names are tagged correctly

--- c2rust-transpile/src/renamer.rs
+++ c2rust-transpile/src/renamer.rs
@@ -105,12 +105,14 @@ impl<T: Clone + Eq + Hash> Renamer<T> {
     }
 
     pub fn pick_name(&mut self, basename: &str) -> String {
+        assert!(basename.starts_with("c2rust_") || basename.starts_with("C2Rust_"));
         self.pick_name_in_scope(basename, None)
     }
 
     /// Permanently assign a name that doesn't collide with anything
     /// currently in scope, and also never goes out of scope
     pub fn pick_name_root(&mut self, basename: &str) -> String {
+        assert!(basename.starts_with("c2rust_") || basename.starts_with("C2Rust_"));
         self.pick_name_in_scope(basename, Some(0))
     }
 
