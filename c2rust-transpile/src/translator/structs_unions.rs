9ff10984804108f9c5ff082c9b92a1f597c4c217
Rua <ruawhitepaw@gmail.com>
2026-08-15 18:39:53 +0200

    transpile: Replace `ExprContext::is_used` and `is_unused` with direct access

--- c2rust-transpile/src/translator/structs_unions.rs
+++ c2rust-transpile/src/translator/structs_unions.rs
@@ -1054,7 +1054,7 @@ impl<'a> Translation<'a> {
         lrvalue: LRValue,
         override_ty: Option<CQualTypeId>,
     ) -> TranslationResult<WithStmts<Box<Expr>>> {
-        if ctx.is_unused() {
+        if !ctx.is_used {
             return self.convert_expr(ctx, expr, None);
         }
 
